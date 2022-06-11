cb8a7cbd4fa67c5001421c99e354e708049905d7
Taufik Hidayat Harahap <hrptaufik42@gmail.com>
2022-06-11 21:22:09 +0700

    Membuat fungsi promise

--- external.js
+++ external.js
@@ -1,51 +1,51 @@
 // ! JANGAN DIMODIFIKASI
 const promiseTheaterIXX = async () => {
-  await new Promise((r) => setTimeout(r, 200));
+    await new Promise((r) => setTimeout(r, 200));
 
-  return new Promise((resolve) => {
-    const hasilSetelahMenonton = [
-      {
-        namaFilm: 'World of the married',
-        hasil: 'marah',
-      },
-      {
-        namaFilm: 'Inside Out',
-        hasil: 'tidak marah',
-      },
-      {
-        namaFilm: 'Nanti Kita Cerita Tentang Hari Ini',
-        hasil: 'marah',
-      },
-    ];
+    return new Promise((resolve) => {
+        const hasilSetelahMenonton = [
+            {
+                namaFilm: "World of the married",
+                hasil: "marah",
+            },
+            {
+                namaFilm: "Inside Out",
+                hasil: "tidak marah",
+            },
+            {
+                namaFilm: "Nanti Kita Cerita Tentang Hari Ini",
+                hasil: "marah",
+            },
+        ];
 
-    return resolve(hasilSetelahMenonton);
-  });
+        return resolve(hasilSetelahMenonton);
+    });
 };
 
 const promiseTheaterVGC = async () => {
-  await new Promise((r) => setTimeout(r, 200));
+    await new Promise((r) => setTimeout(r, 200));
 
-  return new Promise((resolve) => {
-    const hasilSetelahMenonton = [
-      {
-        namaFilm: 'KKN di Desa Penari',
-        hasil: 'marah',
-      },
-      {
-        namaFilm: 'Kukira Kau Rumah',
-        hasil: 'tidak marah',
-      },
-      {
-        namaFile: 'The Other Side',
-        hasil: 'marah',
-      },
-    ];
+    return new Promise((resolve) => {
+        const hasilSetelahMenonton = [
+            {
+                namaFilm: "KKN di Desa Penari",
+                hasil: "marah",
+            },
+            {
+                namaFilm: "Kukira Kau Rumah",
+                hasil: "tidak marah",
+            },
+            {
+                namaFile: "The Other Side",
+                hasil: "marah",
+            },
+        ];
 
-    return resolve(hasilSetelahMenonton);
-  });
+        return resolve(hasilSetelahMenonton);
+    });
 };
 
 module.exports = {
-  promiseTheaterIXX,
-  promiseTheaterVGC,
+    promiseTheaterIXX,
+    promiseTheaterVGC,
 };
--- promise.js
+++ promise.js
@@ -1,8 +1,42 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+
+const promiseOutput = async (ekspresi) => {
+    const dataMarah = [];
+    const dataTidakMarah = [];
+    const dataTidakAda =
+        '"Data tidak ditemukan, periksa kembali penulisan kata"';
+
+    await promiseTheaterIXX().then((data1) => {
+        data1.filter((elemen) => {
+            if (elemen.hasil === "marah") {
+                dataMarah.push(elemen.hasil);
+            } else {
+                dataTidakMarah.push(elemen.hasil);
+            }
+        });
+    });
+
+    await promiseTheaterVGC().then((data2) => {
+        data2.filter((elemen) => {
+            if (elemen.hasil === "marah") {
+                dataMarah.push(elemen.hasil);
+            } else {
+                dataTidakMarah.push(elemen.hasil);
+            }
+        });
+    });
+
+    if (ekspresi === "marah") {
+        return dataMarah.length;
+    } else if (ekspresi === "tidak marah") {
+        return dataTidakMarah.length;
+    } else {
+        return dataTidakAda;
+    }
+};
 
 module.exports = {
-  promiseOutput,
+    promiseOutput,
 };
